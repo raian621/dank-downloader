512756af0f73bd7def59037a942e31de8da1d901
Bell, Ryan <ryan.bell62101@gmail.com>
2023-02-23 13:10:29 -0600

    added requirements.txt

--- main.py
+++ main.py
@@ -1,3 +1,10 @@
 from pytube import YouTube
-yt = YouTube('https://www.youtube.com/watch?v=dQw4w9WgXcQ')
-yt.streams.filter(progressive=True, file_extension='mp4').order_by('resolution').desc().first().download()
+
+
+def main():
+    yt = YouTube('https://www.youtube.com/watch?v=dQw4w9WgXcQ')
+    yt.streams.filter(progressive=True, file_extension='mp4').order_by('resolution').desc().first().download()
+
+
+if __name__ == '__main__':
+    main()
