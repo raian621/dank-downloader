1beb6801a58414b2b870a4a2c5d5d8d0c8df0dcc
Bell, Ryan <ryan.bell62101@gmail.com>
2023-02-23 13:27:20 -0600

    added deleted class files  back as python class files

---
+++ Media.py
@@ -0,0 +1,6 @@
+class Media:
+    def __init__(self, playback_length, media_url, filepath, media_data):
+        self.playback_length = playback_length
+        self.media_url = media_url
+        self.filepath = filepath
+        self.media_data = media_data
---
+++ MediaData.py
@@ -0,0 +1,6 @@
+class MediaData:
+    def __init__(self, media_data):
+        self.media_data = media_data
+
+	def loadMedia(path):
+        pass
