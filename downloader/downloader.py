9f77e19a9f415d96b325e02b6cda5e93667288a2
Bell, Ryan <ryan.bell62101@gmail.com>
2023-02-27 15:25:53 -0600

    got 'audio downloads' working

--- downloader/downloader.py
+++ downloader/downloader.py
@@ -1,4 +1,6 @@
 from pytube import YouTube
+import moviepy.editor as mp
+import os
 
 def on_progress_callback(stream, chunk, bytes_remaining):
     total_size = stream.filesize
@@ -25,30 +27,45 @@ def get_pytube_streams(
     is_dash=None,
 ):
     yt = YouTube(url)
-    return yt.streams.filter(
-        fps=fps,
-        res=res,
-        resolution=resolution,
-        mime_type=mime_type,
-        type=type,
-        subtype=subtype,
-        file_extension=file_extension,
-        abr=abr,
-        bitrate=bitrate,
-        video_codec=video_codec,
-        audio_codec=audio_codec,
-        only_audio=only_audio,
-        only_video=only_video,
-        progressive=progressive,
-        adaptive=adaptive,
-        is_dash=is_dash
-    )
+    streams = None
+
+    try:
+        streams = yt.streams.filter(
+            fps=fps,
+            res=res,
+            resolution=resolution,
+            mime_type=mime_type,
+            type=type,
+            subtype=subtype,
+            file_extension=file_extension,
+            abr=abr,
+            bitrate=bitrate,
+            video_codec=video_codec,
+            audio_codec=audio_codec,
+            only_audio=only_audio,
+            only_video=only_video,
+            progressive=progressive,
+            adaptive=adaptive,
+            is_dash=is_dash
+        )
+    except Exception as e:
+        print(e.with_traceback)
+
+    return streams
 
 def download_audio(url: str, file_extension=None, file_destination=None):
-    pass
+    streams = get_pytube_streams(url, file_extension)
+    
+    if streams == None:
+        video_file_path = download_video(url, file_extension="mp4")
+        video = mp.VideoFileClip(video_file_path)
+        video.audio.write_audiofile(video_file_path.replace("mp4", file_extension))
+        os.remove(video_file_path)
+
+        return
 
 def download_video(url: str, file_extension=None, resolution=None, fps=None, file_destination=None):
     streams = get_pytube_streams(url, file_extension=file_extension, resolution=resolution, progressive=True, fps=fps)
     stream = streams.order_by('resolution').desc().first()
 
-    stream.download(file_destination)
+    return stream.download(file_destination)
--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 from downloader import download_video, download_audio
+from database import *
 
 def main():
-    download_video('https://www.youtube.com/shorts/tv2kXFrXucQ', file_extension="mp4")
+    download_audio('https://www.youtube.com/watch?v=dQw4w9WgXcQ', file_extension="mp3")
 
 if __name__ == '__main__':
     main()
