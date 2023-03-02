9264903cf56e0bbeac951dd0f76e35061099d1a8
Bell, Ryan <ryan.bell62101@gmail.com>
2023-03-02 12:10:06 -0600

    program now downloads video and audio in parallel

--- downloader/downloader.py
+++ downloader/downloader.py
@@ -3,6 +3,7 @@ import moviepy.editor as mp
 import ffmpeg
 import os
 import re
+from threading import Thread
 
 from database import *
 
@@ -19,6 +20,10 @@ def on_progress_callback(stream, chunk, bytes_remaining):
     percent_complete = (total_size - bytes_remaining) / total_size
     progress_line_end = "\r" if percent_complete < 1 else "\n"
     print("Download {:.2f}% complete".format(percent_complete * 100), end=progress_line_end)
+
+
+def download_stream(stream, video_file_path):
+    stream.download(filename=video_file_path)
 
 
 def get_pytube_streams(
@@ -63,19 +68,25 @@ def get_pytube_streams(
             adaptive=adaptive,
             is_dash=is_dash
         )
-
-        print(streams)
     except Exception as e:
         print(e)
 
     return (streams, yt.length)
 
 
-def download_video(url: str, file_extension=None, resolution=None, fps=None, file_destination=DOWNLOAD_DIRECTORY, register=True, title=None, subtitle=None):
-    
-    streams, length = get_pytube_streams(url, file_extension=file_extension, progressive=False, fps=fps)
+def download_video(
+        url: str, 
+        file_extension=None, 
+        resolution=None, 
+        fps=None,
+        file_destination=DOWNLOAD_DIRECTORY,
+        register=True,
+        title=None,
+        subtitle=None
+):
+    streams, length = get_pytube_streams(url, file_extension=file_extension, fps=fps)
 
-    video_stream = streams.filter(resolution=resolution).desc().first()
+    video_stream = streams.filter(only_video=True, resolution=resolution).desc().first()
     audio_stream = streams.filter(only_audio=True).order_by('bitrate').desc().first()
 
     print(video_stream)
@@ -85,23 +96,39 @@ def download_video(url: str, file_extension=None, resolution=None, fps=None, fil
         ext = re.findall("\.(.*)", video_file_path)
         if ext:
             file_extension = ext[0]
+    
+    title = video_stream.title
+    file_path = os.path.join(file_destination, f"{title}.{file_extension}")
 
-    video_file_path = video_stream.download(file_destination)
-    audio_file_path = None
     if audio_stream:
-        audio_file_path = audio_stream.download(file_destination,
-                video_file_path.replace(f".{file_extension}", f"_audio.mp4"))
-        video_file_path_old = video_file_path
+        video_file_path = os.path.join(file_destination, f"{title}_video.{file_extension}")
+        audio_file_path = os.path.join(file_destination, f"{title}_audio.{file_extension}")
+
+        video_thread = Thread(
+            target=download_stream, 
+            args=(video_stream, video_file_path,)
+        )
+        audio_thread = Thread(
+            target=download_stream, 
+            args=(audio_stream, audio_file_path,)
+        )
+
+        video_thread.start()
+        audio_thread.start()
+        video_thread.join()
+        audio_thread.join()
+
         # combine video and audio files.
         # I swear this was the only way to get HD video
-        video_file_path = video_file_path.replace(f".{file_extension}", f"_video.mp4")
-        os.rename(video_file_path_old, video_file_path)
         video = ffmpeg.input(video_file_path)
         audio = ffmpeg.input(audio_file_path)
-        ffmpeg.output(video, audio, video_file_path_old).run()
+        ffmpeg.output(video, audio, file_path).run()
+
         os.remove(audio_file_path)
         os.remove(video_file_path)
-
+    else:
+        download_stream(video_stream, file_path)
+    
     if register:
         Session = sessionmaker(engine)
         with Session() as session:
@@ -121,7 +148,14 @@ def download_video(url: str, file_extension=None, resolution=None, fps=None, fil
     return video_file_path
 
 
-def download_audio(url: str, file_extension="mp4", file_destination=DOWNLOAD_DIRECTORY, register=True, title=None, subtitle=None):
+def download_audio(
+        url: str, 
+        file_extension="mp3", 
+        file_destination=DOWNLOAD_DIRECTORY,
+        register=True,
+        title=None,
+        subtitle=None
+):
     streams, length = get_pytube_streams(url, file_extension)
     
     file_path = None
