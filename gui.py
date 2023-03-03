97bcf3332d8b50c99b591f406fb7cd2892ba9099
p_picklelol <111909631+bloxycolafan@users.noreply.github.com>
2023-03-03 13:06:13 -0600

    added buttons within the gui
    still not connected to main and have no functionality as of now

--- gui.py
+++ gui.py
@@ -1,33 +1,48 @@
-# this is test code not intended for the release
 # this is not connected to main as of now
 
 # TO DO
-# Make custom theme
-# Make homepage
-# Add downloading
+# Add functionality to the buttons
+# Make scrolling areas
+# Add downloading functionality
+
+# IMPORTANT READ BEFORE EDITING
+# the keys to the buttons are in the format -example_button- keep this consistant
+# all images go into the assets folder access it with ./assets
+# limit the use of whitespace to make text not as long in the window layout
+
 
 import PySimpleGUI as sg
 
-# set button images
-download_icon = './assets/download.png'
-plus_icon = './assets/download.png'
+# set images
+# IMPORTANT put all images in the assets folder
+doge_image = './assets/dankdownloader.ico'
+download_image = './assets/download64x64.png'
+plus_image = './assets/plus64x64.png'
 
+# this is a placeholder and can be changed inside the window
 sg.theme('DarkBlue1')
 
-def create_window():
+def create_main_window():
     # this is what appears inside the window that is created
-    layout = [  [sg.Text('Playlists', font='Roboto 20', text_color='#A7E5FF')],
-                [sg.Text('Media', font='Roboto 20', text_color='#A7E5FF')]  ]
 
-    window = sg.Window('Dank Downloader', layout, icon='./assets/dankdownloader.ico', size=(900,750))
+                # playlist area
+    layout = [  [sg.Text('Playlists', font='Roboto 20', text_color='#A7E5FF'),
+                 sg.Button(key='-download_playlists-', button_color=(sg.theme_background_color(), sg.theme_background_color()), image_filename=download_image, image_size=(30, 25), image_subsample=2, border_width=0),
+                 sg.Button(key='-add_playlists-', button_color=(sg.theme_background_color(), sg.theme_background_color()), image_filename=plus_image, image_size=(30, 30), image_subsample=2, border_width=0)],
+                # media area
+                [sg.Text('Media    ', font='Roboto 20', text_color='#A7E5FF'),
+                 sg.Button(key='-download_media-', button_color=(sg.theme_background_color(), sg.theme_background_color()), image_filename=download_image, image_size=(30, 25), image_subsample=2, border_width=0),
+                 sg.Button(key='-add_media-', button_color=(sg.theme_background_color(), sg.theme_background_color()), image_filename=plus_image, image_size=(30, 30), image_subsample=2, border_width=0)]  ]
+
+    window = sg.Window('Dank Downloader', layout, icon=doge_image, size=(900,750))
     return window
 
-window = create_window()
+window = create_main_window()
 
 while True:
     event, values = window.read()
-    if event == sg.WIN_CLOSED or event == 'Cancel':
+    if event == sg.WIN_CLOSED:
         break
-    print('You entered', values[0])
+    print('You clicked a button')
 
 window.close()
